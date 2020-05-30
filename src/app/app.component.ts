8c12841a9c41f051964bbb70a221cc113c046109
thiagolvlsantos <thiago.lvl.santos@gmail.com>
2020-05-30 16:38:00 -0300

    Return Stackblitz problems with Gitlab.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,16 @@
-import { Component, VERSION,OnInit } from '@angular/core';
+import { Component, VERSION } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent implements OnInit  {
-  name = 'File upload in Angular ' + VERSION.full;
-  filesToUpload: FileList = null;
-  files: any[] = [];
+export class AppComponent  {
+  name = 'Autoresize <textarea> in Angular ' + VERSION.full;
+  size = 20;
 
-  constructor(){
-  }
-
-  ngOnInit() {
-  }
-
-  handleFileInput(event: any) {
-    let target = event.target as HTMLInputElement;
-    this.filesToUpload = target.files;
-    this.files = [];
-    for(let i = 0; i < this.filesToUpload.length;i++) {
-      let f = this.filesToUpload.item(i);
-      this.files.push(f);
-      let fileReader = new FileReader();
-      fileReader.onload = (e) => {
-        this.files[i].meta = f;
-        this.files[i].content = fileReader.result;
-      };
-      fileReader.readAsText(this.files[i]);
-    }
+  rows(str: string) {
+    let tmp = Math.floor(str.length/this.size);
+    return tmp > 0 ? tmp + 2: 1;
   }
 }
